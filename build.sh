be0c320a96a1ee8478a4913d4e317e186d3ba094
Chris Wren <chriswrendeveloper@gmail.com>
2024-12-09 20:53:49 +0000

    Port kernal to Rust

--- build.sh
+++ build.sh
@@ -2,16 +2,7 @@
 
 set -ex
 
-CROSS_COMPILE=~/opt/cross/bin/i686-elf-
-
-# Assemble boot.s
-"${CROSS_COMPILE}"as boot.s -o boot.o
-
-# Compile kernal
-"${CROSS_COMPILE}"gcc -c kernel.c -o kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra
-
-# Link kernal
-"${CROSS_COMPILE}"gcc -T linker.ld -o myos.bin -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
+cargo build
 
 if grub-file --is-x86-multiboot myos.bin; then
   echo multiboot confirmed
@@ -26,7 +17,7 @@ fi
 
 mkdir -p isodir/boot/grub
 
-cp myos.bin isodir/boot/myos.bin
+cp target/target/os-rust isodir/boot/myos.bin
 cp grub.cfg isodir/boot/grub/grub.cfg
 grub-mkrescue -o myos.iso isodir
 
